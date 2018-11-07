80bf6e73993692234440440bd2170ee71591a64e
Brenden O Reeves <reevesb@otc.edu>
2018-11-07 17:31:52 -0600

    Added Demo Files

---
+++ public/intro/demo/index.js
@@ -0,0 +1,28 @@
+// Global Variables
+var db;
+var messages;
+
+// TODO: Update UI
+function authStateObserver(user) {
+    if (user) {
+
+        loadMessages();
+    } else {
+
+    }
+}
+
+// TODO: Post Document to Collection
+function sendMessage(e){
+
+}
+
+// TODO: Load Sorted Collection
+function loadMessages() {
+
+}
+
+// TODO: Display Message to User
+function displayMessage(key, name, text, uid) {
+
+}
